6bacb78c5fe7bbf04dbb098fa35b6f7bfac367d8
Syauqi <sauki084@gmail.com>
2025-10-21 22:19:08 +0700

    Handle login

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -35,4 +35,20 @@ class JWTAuthController extends Controller
 
         return response()->json(compact('user', 'token'), 201);
     }
+
+    // Handle Login
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
 }
